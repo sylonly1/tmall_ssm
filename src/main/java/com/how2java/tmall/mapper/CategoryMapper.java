086c98e2f0504bce2f119deb4a70498e7bf13fad
Syl <2534041854@qq.com>
2020-04-08 23:35:13 +0800

    天猫商城模拟

---
+++ src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.how2java.tmall.mapper;
+
+import com.how2java.tmall.pojo.Category;
+
+import java.util.List;
+
+public interface CategoryMapper {
+    List<Category> list();
+    int total();
+    void add(Category category);
+    void delete(int id);
+    Category get(int id);
+    void update(Category category);
+}
---
+++ src/main/java/com/how2java/tmall/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.how2java.tmall.service;
+
+import com.how2java.tmall.pojo.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> list();
+    int total();
+    void add(Category category);
+    void delete(int id);
+    Category get(int id);
+    void update(Category category);
+}
